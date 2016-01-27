382266a7346e6e25b6daba75f1339b996c8f66b7
Konstantin Misyutin <ikeeip@arello-mobile.com>
2016-01-27 09:18:25 +0600

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='confluence-publisher',
-    version='1.0.0',
+    version='1.1.0',
     packages=find_packages(exclude=['tests', 'tests.*']),
     include_package_data=True,
     license='MIT',
